dda7da83b63abcea19ab1a4e65e59e85afbb2803
Sean Lightfoot <seanalexander360@gmail.com>
2021-10-13 10:38:45 -0500

    hero

--- url-app/src/components/Hero.js
+++ url-app/src/components/Hero.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Illustration from "../images/illustration-working.svg";
 
 export default function Hero() {
-    return (
-        <div>
-            <img alt='hero' />
-            <h1>More than just shorter links</h1>
-            <p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
-            <button>Get Started</button>
-        </div>
-    )
+  return (
+    <div className="hero">
+      <img className="illustration" src={Illustration} alt="hero" />
+      <div className="hero-text">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <span className="hero-button">Get Started</span>
+      </div>
+    </div>
+  );
 }
